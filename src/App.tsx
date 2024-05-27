41658c8983aa9475e1014569f2f1fd32afb3bfb4
Slobodan Vujovic <slobodan.vujovic@zuehlke.com>
2024-05-27 11:00:34 +0200

    Setup host app

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, lazy, Suspense } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+// Name of the component itself must match the name of the component we exposed in mfe-body/vite.config.ts in "exposes" property.
+import RemoteBody from 'tralalaBody/RemoteBody';
+
+// import RemoteFooter from 'tralalaFooter/RemoteFooter';
+// This way of importing component can be used for both static and dynamic URLs definition in vite.config.ts.
+const RemoteFooter = lazy(() => import('tralalaFooter/RemoteFooter'));
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href='https://vitejs.dev' target='_blank'>
+          <img src={viteLogo} className='logo' alt='Vite logo' />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href='https://react.dev' target='_blank'>
+          <img src={reactLogo} className='logo react' alt='React logo' />
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <div className='card'>
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
+
+      <div id='remoteFooter'>
+        <div>REMOTE FOOTER</div>
+        <Suspense fallback={<div>Loading...</div>}>
+          <RemoteFooter />
+        </Suspense>
+      </div>
+
+      <div id='remoteBody'>
+        <div>REMOTE BODY</div>
+        <RemoteBody />
+      </div>
+
+      <div>{import.meta.env.VITE_TRALALA_BODY_URL}</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,48 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import federation from '@originjs/vite-plugin-federation';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    federation({
+      name: 'remoteModules',
+      remotes: [
+        {
+          // This name can be anything, but it will be used as the module name from whom we will import remote components in
+          // some host component (App.tsx). We also need to declare this module in modules.ds.ts.
+          // Each remote module name must be unique.
+          tralalaBody: {
+            // Static (hardcoded) URL to the remote module. In this case externalType must have value 'url'.
+            // external: 'http://localhost:5173/assets/remoteBody.js',
+
+            // Dynamic URL to the remote module. In this case externalType must have value 'promise'.
+            external: `Promise.resolve(import.meta.env.VITE_TRALALA_BODY_URL)`,
+            from: 'vite',
+            externalType: 'promise',
+          },
+        },
+        {
+          tralalaFooter: {
+            // external: 'http://localhost:5174/assets/remoteFooter.js',
+            external: `Promise.resolve(import.meta.env.VITE_TRALALA_FOOTER_URL)`,
+            from: 'vite',
+            externalType: 'promise',
+          },
+        },
+      ],
+      shared: ['react', 'react-dom'],
+    }),
+  ],
+  preview: {
+    host: 'localhost',
+    port: 5000,
+    strictPort: true,
+  },
+  build: {
+    target: 'esnext',
+    minify: false,
+    cssCodeSplit: false,
+  },
+});
