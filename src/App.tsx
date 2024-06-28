c3dcc8bc6286fe8ba27e42f4de75dcb6c69f4d59
Slobodan Vujovic <slobodan.vujovic@zuehlke.com>
2024-06-28 16:18:17 +0200

    Remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { useState, lazy, Suspense } from 'react';
 // Docs: https://software-engineering-corner.zuehlke.com/micro-frontend-module-federation-with-vite-for-react
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 // Name of the component itself must match the name of the component we exposed in mfe-body/vite.config.ts in "exposes" property.
-import RemoteBody from 'tralalaBody/RemoteBody';
+import RemoteBody from 'tralalaBody/RemoteBody1';
+import RemoteBody2 from 'tralalaBody/RemoteBody2';
+// import vujo-component from 'tralalaBody/vujo-component';
 
 // import RemoteFooter from 'tralalaFooter/RemoteFooter';
 // This way of importing component can be used for both static and dynamic URLs definition in vite.config.ts.
@@ -14,36 +14,38 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
+    <div id='container'>
+      <div id='header'>
+        <h1>Host app</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
 
-      <div id='remoteFooter'>
+      <div id='sideBar'>
+        <div className='card'>
+          <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        </div>
+      </div>
+
+      <div id='body'>
+        <h1>Remote repo at {import.meta.env.VITE_TRALALA_BODY_URL}</h1>
+        <div id='remoteBodyComponent1'>
+          <RemoteBody />
+        </div>
+
+        <div id='remoteBodyComponent2'>
+          <RemoteBody2 />
+        </div>
+      </div>
+
+      <div id='footer'>
         <Suspense fallback={<div>Loading...</div>}>
           <RemoteFooter />
         </Suspense>
       </div>
 
-      <div id='remoteBody'>
-        <RemoteBody />
-      </div>
-
-      <div>{import.meta.env.VITE_TRALALA_BODY_URL}</div>
-    </>
+      {/* <div id='remoteWebComponent'>
+        <vujo-component></vujo-component>
+      </div> */}
+    </div>
   );
 }
 
